afd02648e0a432663e78b442ea9e30d9e082ae1c
Roconx <69419903+Roconx@users.noreply.github.com>
2022-11-12 08:51:38 +0100

    Removed 1 second delay from keyboard_controller

--- src/keyboard_controller.rs
+++ src/keyboard_controller.rs
@@ -1,10 +1,6 @@
 use enigo::{Enigo, Key, KeyboardControllable};
-use std::thread;
-use std::time::Duration;
 
 pub fn send_message(message: &str) {
-    let one_second = Duration::from_secs(1);
-    thread::sleep(one_second);
 
     let message = format!("/all {}", message);
 
