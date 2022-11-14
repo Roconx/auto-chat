6d683437f97d7f5b07d7e8ad85f6c989f17b11a5
Roconx <arnaurocap@gmail.com>
2022-11-14 11:58:08 +0100

    Finally works!

--- src/keyboard_controller.rs
+++ src/keyboard_controller.rs
@@ -1,6 +1,7 @@
 use crate::parser;
 use enigo::{Enigo, KeyboardControllable};
 use tfc::{traits::*, Context, Key};
+use std::{thread, time};
 
 pub enum Action<'a> {
     Message(&'a Vec<String>),
@@ -11,10 +12,14 @@ pub enum Action<'a> {
 pub fn send_message(message: &str) {
     // Sends a message to chat
     let mut enigo = Enigo::new();
+    let mut ctx = Context::new().unwrap();
+    let delay = time::Duration::from_millis(100);
 
     enigo.key_click(enigo::Key::Return);
 
-    enigo.key_sequence(message);
+    ctx.unicode_string(message).unwrap();
+
+    thread::sleep(delay);
 
     enigo.key_click(enigo::Key::Return);
 }
@@ -23,7 +28,9 @@ pub fn press_key(key: Key) {
     // Presses the key
     let mut ctx = Context::new().unwrap();
 
-    ctx.key_click(key).unwrap();
+    ctx.key_click(Key::Period).unwrap();
+
+    println!("Death detected");
 }
 
 pub fn perform_action(action: &Action) {
@@ -31,8 +38,8 @@ pub fn perform_action(action: &Action) {
         Action::Message(file) => {
             let message = parser::get_random(file);
             send_message(&message);
-        }
-        Action::Mastery => press_key(Key::Period),
+        },
+        Action::Mastery => send_message("/masterybadge"),
         Action::Surrender => send_message("/ff"),
     };
 }
