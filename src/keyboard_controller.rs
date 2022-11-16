8e48bffbd447dcd930548d8f0c3e1f86179b21e9
Roconx <69419903+Roconx@users.noreply.github.com>
2022-11-16 14:13:49 +0100

    Removed delay

--- src/keyboard_controller.rs
+++ src/keyboard_controller.rs
@@ -1,6 +1,5 @@
 use crate::parser;
 use enigo::{Enigo, KeyboardControllable};
-use std::{thread, time};
 use tfc::{traits::*, Context};
 
 pub enum Action<'a> {
@@ -13,13 +12,10 @@ pub fn send_message(message: &str) {
     // Sends a message to chat
     let mut enigo = Enigo::new();
     let mut ctx = Context::new().unwrap();
-    let delay = time::Duration::from_millis(100);
 
     enigo.key_click(enigo::Key::Return);
 
     ctx.unicode_string(message).unwrap();
-
-    thread::sleep(delay);
 
     enigo.key_click(enigo::Key::Return);
 }
